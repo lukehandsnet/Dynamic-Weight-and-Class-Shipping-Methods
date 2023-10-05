524607968322c7e922496ac7eac4a85dda58c196
luke-hands <luke@lukehands.net>
2023-10-05 15:24:23 +1000

    Update admin-settings-handler.php

--- admin-settings-handler.php
+++ admin-settings-handler.php
@@ -82,14 +82,31 @@ class Admin_Settings_Handler {
     public function display_shipping_methods() {
         $shipping_zones = \WC_Shipping_Zones::get_zones();
         echo '<h3>Available Shipping Methods</h3>';
-        foreach( $shipping_zones as $zone ) {
+        $stored_settings = get_option('asm_settings', []);
+    
+        foreach ($shipping_zones as $zone) {
             echo '<strong>' . esc_html($zone['zone_name']) . '</strong><ul>';
-            foreach( $zone['shipping_methods'] as $method ) {
-                echo '<li>' . esc_html($method->title) . '</li>';
+            foreach ($zone['shipping_methods'] as $method) {
+                $min_weight = $stored_settings[$method->id]['min_weight'] ?? '';
+                $max_weight = $stored_settings[$method->id]['max_weight'] ?? '';
+                
+                echo '<li>';
+                echo '<label>' . esc_html($method->title) . '</label>';
+                echo ' - Min Weight: <input type="text" name="asm_settings[' . esc_attr($method->id) . '][min_weight]" value="' . esc_attr($min_weight) . '">';
+                echo ' - Max Weight: <input type="text" name="asm_settings[' . esc_attr($method->id) . '][max_weight]" value="' . esc_attr($max_weight) . '">';
+                
+                // Display available classes with checkboxes
+                $shipping_classes = \WC()->shipping->get_shipping_classes();
+                foreach ($shipping_classes as $class) {
+                    $is_checked = in_array($class->term_id, $stored_settings[$method->id]['classes'] ?? []) ? 'checked' : '';
+                    echo '<br>&nbsp;&nbsp;<input type="checkbox" name="asm_settings[' . esc_attr($method->id) . '][classes][]" value="' . esc_attr($class->term_id) . '" ' . $is_checked . '> ' . esc_html($class->name);
+                }
+                echo '</li>';
             }
             echo '</ul>';
         }
     }
+    
 
     public function display_shipping_classes() {
         $shipping_classes = \WC()->shipping->get_shipping_classes();
